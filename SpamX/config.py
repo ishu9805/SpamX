83854eb0934c680256f9ebd0b4d79b86f39b4f43
ishu9805 <abhishek47903@gmail.com>
2024-07-01 14:01:01 +0530

    Update config.py

--- SpamX/config.py
+++ SpamX/config.py
@@ -6,32 +6,32 @@ from dotenv import load_dotenv
 if os.path.exists(".env"):
     load_dotenv(".env")
 
-API_ID = int(env("API_ID", None))
+API_ID = env("API_ID", None)
 if API_ID is None:
     LOGS.error("Please set your API_ID!")
     quit(1)
 
-API_HASH = str(env("API_HASH", None))
+API_HASH = env("API_HASH", None)
 if API_HASH is None:
     LOGS.error("Please set your API_HASH!")
     quit(1)
 
-ASSISTANT_TOKEN = str(env("ASSISTANT_TOKEN", None))
+ASSISTANT_TOKEN = env("ASSISTANT_TOKEN", None)
 if ASSISTANT_TOKEN is None:
     LOGS.error("Please set your ASSISTANT_TOKEN!")
     quit(1)
 
-OWNER_ID = int(env("OWNER_ID", None))
+OWNER_ID = env("OWNER_ID", None)
 if OWNER_ID is None:
     LOGS.error("Please set your OWNER_ID!")
     quit(1)
 
-DATABASE_URL = str(env("DATABASE_URL", None))
+DATABASE_URL = env("DATABASE_URL", None)
 if DATABASE_URL is None:
     LOGS.error("Please set your DATABASE_URL!")
     quit(1)
 
-LOGGER_ID = str(env("LOGGER_ID", None))
+LOGGER_ID = env("LOGGER_ID", None)
 if LOGGER_ID is None:
     LOGS.error("Please set your LOGGER_ID! (make a private group add assistant in that group & promote as Admin!)")
     quit(1)
